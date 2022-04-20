a7af90e6e7f2647222c692c10779541fd221574d
NanaGlutamate <30829895+NanaGlutamate@users.noreply.github.com>
2022-04-20 20:18:04 +0800

    Create vectorn.hpp
    上传vectorn，在array基础上封装了常用向量运算

---
+++ vectorn.hpp
@@ -0,0 +1,80 @@
+#ifndef __VECTORN_HPP__
+#define __VECTORN_HPP__
+
+#include <array>
+
+template <typename E, int N>
+class VectorN {
+public:
+    std::array<E, N> data;
+
+    // template <typename... Args>
+    // VectorN(Arg...args): data({std::forward<std::enable_if<sizeof...(Args)==N, E>::type>(args),...}) {};
+    // VectorN():data() {};
+    // VectorN(const VectorN& other) : data(other.data) {};
+    // VectorN(VectorN&& other) : data(other.data) {};
+    
+    VectorN& operator=(const VectorN& other) {
+        data = other.data;
+        return *this;
+    };
+    VectorN& operator+=(const VectorN& other) {
+        for (int i = 0; i < N; i++) {
+            data[i] += other.data[i];
+        }
+        return *this;
+    };
+    VectorN& operator-=(const VectorN& other) {
+        for (int i = 0; i < N; i++) {
+            data[i] -= other.data[i];
+        }
+        return *this;
+    };
+    VectorN& operator*=(const E& other) {
+        for (int i = 0; i < N; i++) {
+            data[i] *= other;
+        }
+        return *this;
+    };
+    VectorN& operator/=(const E& other) {
+        for (int i = 0; i < N; i++) {
+            data[i] /= other;
+        }
+        return *this;
+    };
+    VectorN operator+(const VectorN& other) const {
+        VectorN result = *this;
+        result += other;
+        return result;
+    };
+    VectorN operator-(const VectorN& other) const {
+        VectorN result = *this;
+        result -= other;
+        return result;
+    };
+    VectorN operator*(const E& other) const {
+        VectorN result = *this;
+        result *= other;
+        return result;
+    };
+    VectorN operator/(const E& other) const {
+        VectorN result = *this;
+        result /= other;
+        return result;
+    };
+    E& operator[](int i) const {
+        return data[i];
+    };
+    E dot(const VectorN& other) const {
+        E result = 0;
+        for (int i = 0; i < N; i++) {
+            result += data[i] * other[i];
+        }
+        return result;
+    };
+    E norm() const {
+        return sqrt(dot(*this));
+    };
+};
+
+#endif
